b2819745f4a69866d47ea32896d79fc53711c641
Shay Reichert <shay.reichert@outlook.fr>
2022-02-12 00:35:57 +0100

    Gallery

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import './Gallery.scss'
+
+class Gallery extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeIndex: 0,
+    }
+  }
+
+  updateIndex(newIndex) {
+    const numberOfSlides = this.props.pictures.length
+
+    if (newIndex < 0) {
+      newIndex = numberOfSlides - 1
+    } else if (newIndex >= numberOfSlides) {
+      newIndex = 0
+    }
+    this.setState({ activeIndex: newIndex })
+  }
+
+  render() {
+    const { pictures } = this.props
+
+    return (
+      <section className="gallery-section">
+        <button
+          className="prev"
+          onClick={() => this.updateIndex(this.state.activeIndex - 1)}
+        ></button>
+        <button
+          className="next"
+          onClick={() => this.updateIndex(this.state.activeIndex + 1)}
+        ></button>
+        {/* TODO : */}
+        {/* Pas de buttons s'il n'y a qu'une seule image */}
+        <div
+          className="inner"
+          style={{
+            transform: `translateX(-${this.state.activeIndex * 100}%)`,
+          }}
+        >
+          {pictures.map((picture, index) => (
+            <div key={index} className="item-container">
+              <img src={picture} alt="" className="item" />
+              <div className="position">
+                {index + 1} / {pictures.length}
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Gallery
--- src/pages/SingleLodging.jsx
+++ src/pages/SingleLodging.jsx
@@ -1,23 +1,95 @@
 import React, { Component } from 'react'
-import './Home.scss'
-import Hero from '../components/Hero'
-import heroImage from '.././assets/home_hero.jpg'
+import './SingleLodging.scss'
+import Gallery from '../components/Gallery'
+import data from '../data.json'
 
 class SingleLodging extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      singleLodgingData: {
+        equipments: [],
+        host: {},
+        pictures: [],
+        tags: [],
+      },
+    }
   }
 
   componentDidMount() {
     const { lodgingId } = this.props.match.params
-    console.log(lodgingId)
+    const singleLodging = data.find((lodging) => lodging.id === lodgingId)
+    this.setState({ singleLodgingData: singleLodging })
   }
 
+
   render() {
+    const singleLodging = this.state.singleLodgingData
+    const {
+      pictures,
+      cover,
+      title,
+      location,
+      host,
+      tags,
+      rating,
+      description,
+      equipments,
+    } = singleLodging
+
     return (
       <>
-        <Hero image={heroImage}></Hero>
+
+        <Gallery pictures={pictures} />
+
+
+        {/* Créer composant Gallery (pictures) */}
+
+        <section className="single-content-section">
+          <div className="row">
+            <div className="title-container">
+              <h1>{title}</h1>
+              <p>{location}</p>
+            </div>
+            <div className="host-container">
+              <div className="name">{host.name}</div>
+              <img src={host.picture} alt="host" className="picture" />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="tags-container">
+              {tags.map((tag, index) => (
+                <div key={index} className="tag">
+                  {tag}
+                </div>
+              ))}
+              {/* Créer component <Tag /> */}
+            </div>
+
+            <div className="rating">
+              {[...Array(5)].map((star, index) => {
+                index += 1
+                return (
+                  <span
+                    type="button"
+                    key={index}
+                    className={`star ${index <= rating ? 'full' : ''}`}
+                  >
+                    *{/* Mettre le vrai svg */}
+                  </span>
+                )
+              })}
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="collapse-half">{description}</div>
+            <div className="collapse-half">{equipments}</div>
+            {/* Créer component <CollapseHalf /> */}
+            {/* Cf modèle Component Collapse */}
+          </div>
+        </section>
       </>
     )
   }
