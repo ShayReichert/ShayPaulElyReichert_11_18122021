8241c391ad0e8508eae6e371c8079a4ccce2b122
Shay Reichert <shay.reichert@outlook.fr>
2022-02-18 13:12:52 +0100

    gh-pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import './index.scss'
